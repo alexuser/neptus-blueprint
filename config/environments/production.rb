c35138d058b98f5eeb6aaa35d73bbbbb6216deb0
Gerald Green <geraldgreen@berkeley.edu>
2013-12-02 20:27:42 -0800

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,8 @@ Neptus::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
+  
+  config.assets.precompile += ['rollover.js', 'highcharts.js'] 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
