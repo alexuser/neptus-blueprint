1fe9ebcf9a1db3d1ccd93d7035ae2d50ce8b47c6
Gerald Green <geraldgreen@berkeley.edu>
2013-12-02 19:30:21 -0800

    update the chart logic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,6 +9,16 @@ class WelcomeController < ApplicationController
 	end 
 	
 	def outlook
-	  
+    @graph = LazyHighCharts::HighChart.new('bar') do |f|
+      f.options[:chart][:defaultSeriesType] = "bar"
+      f.series(:name=>"Salary", :data=> [100,12,30])
+      f.series(:name=>"Openings", :data => [10,1,3])
+      f.title({:text=>"Job Outlook"})
+      f.subtitle(:text => 'Source: Wikipedia.org')
+       
+
+      f.options[:xAxis][:categories] = ['Software Engineer','Teacher','Laywer']
+      f.options[:yAxis][:title][:text] = 'Salary ($)' 
+     end
 	end 
 end
