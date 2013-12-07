a1f47ffb76094e4a3b84bce433ae8de7c46191fe
Alex Chen <chen.alex@berkley.edu>
2013-12-06 17:39:59 -0800

    added export functionality for highcharts, cucumber skeleton for forgotten user password: waiting for smtp server

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,12 +10,14 @@ class WelcomeController < ApplicationController
 	
 	def outlook
       @graph = LazyHighCharts::HighChart.new('bar') do |f|
-      f.options[:chart][:defaultSeriesType] = "bar"
-      f.series(:name=>"Salary in $1000", :data=> [65,53,67,65,88,82,91,67])
-      f.series(:name=>"Projected Growth (%)", :data => [26,17,32,17,27,62,30,37])
-      f.title({:text=>"Career Outlook"})
-      f.subtitle(:text => 'Source: Bureau of Labor Statistics')
-      f.options[:xAxis][:categories] = ['Nurse','Elementary School Teacher','Financial Planner', 'Technicial Writer','Actuary','Biomedical Engineer','Software Engineer','Audiologist']
+        f.export(:type => 'image/jpeg')
+        f.options[:chart][:height] = 700
+        f.options[:chart][:defaultSeriesType] = "bar"
+        f.series(:name=>"Salary in $1000", :data=> [65,53,67,65,88,82,91,67])
+        f.series(:name=>"Projected Growth (%)", :data => [26,17,32,17,27,62,30,37])
+        f.title({:text=>"Career Outlook"})
+        f.subtitle(:text => 'Source: Bureau of Labor Statistics')
+        f.options[:xAxis][:categories] = ['Nurse','Elementary School Teacher','Financial Planner', 'Technicial Writer','Actuary','Biomedical Engineer','Software Engineer','Audiologist']
      end
 	end 
 end
