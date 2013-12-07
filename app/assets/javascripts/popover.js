8a509903f546f20eec6a234f85c25915fba0ad1e
Gerald Green <geraldgreen@berkeley.edu>
2013-12-07 09:51:48 -0800

    fix the spelling

--- app/assets/javascripts/popover.js
+++ app/assets/javascripts/popover.js
@@ -1,43 +1,43 @@
-$("#CS-61A").attr("title", "CS61A prerequisite:\n Mathematics 1A (may be taken concurrently); programming experience equivalent to that gained in 3 or the Advanced Placement Computer Science A course.");
+$("#CS-61A").attr("title", "CS 61A prerequisite:\n Mathematics 1A (may be taken concurrently); programming experience equivalent to that gained in 3 or the Advanced Placement Computer Science A course.");
 
-$("#CS-61B").attr("title", "CS61B prerequistie:\n 61A or Engineering 7.");
+$("#CS-61B").attr("title", "CS 61B prerequisite:\n 61A or Engineering 7.");
 
-$("#CS-61C").attr("title", "CS61C prerequistie:\n 61A, along with either 61B or 61BL, or programming experience equivalent to that gained in 9C, 9F, or 9G.");
+$("#CS-61C").attr("title", "CS 61C prerequisite:\n 61A, along with either 61B or 61BL, or programming experience equivalent to that gained in 9C, 9F, or 9G.");
 
-$("#Math-1A").attr("title", "Math-1A prerequistie:\n Three and one-half years of high school math, including trigonometry and analytic geometry, plus a satisfactory grade in one of the following: CEEB MAT test, an AP test, the UC/CSU math diagnostic test, or 32. Consult the mathematics department for details. Students with AP credit should consider choosing a course more advanced than 1A.");
+$("#Math-1A").attr("title", "Math 1A prerequisite:\n Three and one-half years of high school math, including trigonometry and analytic geometry, plus a satisfactory grade in one of the following: CEEB MAT test, an AP test, the UC/CSU math diagnostic test, or 32. Consult the mathematics department for details. Students with AP credit should consider choosing a course more advanced than 1A.");
 
-$("#Math-1B").attr("title", "Math-1B prerequistie:\n Math 1A");
+$("#Math-1B").attr("title", "Math 1B prerequisite:\n Math 1A");
 
-$("#Math-54").attr("title", "Math-54 prerequistie:\n Math 1B");
+$("#Math-54").attr("title", "Math 54 prerequisite:\n Math 1B");
 
-$("#CS-70").attr("title", "CS-70 prerequistie:\n Sophomore mathematical maturity, and programming experience equivalent to that gained in 3 or the Advanced Placement Computer Science A course.");
+$("#CS-70").attr("title", "CS 70 prerequisite:\n Sophomore mathematical maturity, and programming experience equivalent to that gained in 3 or the Advanced Placement Computer Science A course.");
 
-$("#EE-20").attr("title", "EE-20 prerequistie:\n Mathematics 1B.");
+$("#EE-20").attr("title", "EE 20 prerequisite:\n Mathematics 1B.");
 
-$("#EE-40").attr("title", "EE-40 prerequistie:\n Mathematics 1B.");
+$("#EE-40").attr("title", "EE 40 prerequisite:\n Mathematics 1B.");
 
-$("#EE-42").attr("title", "EE-42 prerequistie:\n Mathematics 1B.");
+$("#EE-42").attr("title", "EE 42 prerequisite:\n Mathematics 1B.");
 
-$("#CS-C149").attr("title", "CS C149 prerequistie:\n 20N; Computer Science 61C; Computer Science 70 or Math 55.");
+$("#CS-C149").attr("title", "CS C149 prerequisite:\n 20N; Computer Science 61C; Computer Science 70 or Math 55.");
 
-$("#CS-150").attr("title", "CS 150 prerequistie:\n 61C, Electrical Engineering 40 or 42.");
+$("#CS-150").attr("title", "CS 150 prerequisite:\n 61C, Electrical Engineering 40 or 42.");
 
-$("#CS-152").attr("title", "CS 152 prerequistie:\n 61C.");
+$("#CS-152").attr("title", "CS 152 prerequisite:\n 61C.");
 
-$("#CS-160").attr("title", "CS 160 prerequistie:\n Computer Science 61B or 61BL.");
+$("#CS-160").attr("title", "CS 160 prerequisite:\n Computer Science 61B or 61BL.");
 
-$("#CS-162").attr("title", "CS 162 prerequistie:\n Computer Science 61B, 61C, and 70.");
+$("#CS-162").attr("title", "CS 162 prerequisite:\n Computer Science 61B, 61C, and 70.");
 
-$("#CS-164").attr("title", "CS 164 prerequistie:\n 61B and 61C.");
+$("#CS-164").attr("title", "CS 164 prerequisite:\n 61B and 61C.");
 
-$("#CS-169").attr("title", "CS 169 prerequistie:\n Computer Science 61B and 61C, and either Computer Science 70 or Mathematics 113.");
+$("#CS-169").attr("title", "CS 169 prerequisite:\n Computer Science 61B and 61C, and either Computer Science 70 or Mathematics 113.");
 
-$("#CS-184").attr("title", "CS 184 prerequistie:\n Computer Science 61B or 61BL; programming skills in C, C++, or Java; linear algebra and calculus.");
+$("#CS-184").attr("title", "CS 184 prerequisite:\n Computer Science 61B or 61BL; programming skills in C, C++, or Java; linear algebra and calculus.");
 
-$("#CS-186").attr("title", "CS 186 prerequistie:\n 61B and 61C.");
+$("#CS-186").attr("title", "CS 186 prerequisite:\n 61B and 61C.");
 
-$("#CS-161").attr("title", "CS 161 prerequistie:\n 61C (Machine Structures), plus either 70 (Discrete Mathematics) or Mathematics 55.");
+$("#CS-161").attr("title", "CS 161 prerequisite:\n 61C (Machine Structures), plus either 70 (Discrete Mathematics) or Mathematics 55.");
 
-$("#CS-170").attr("title", "CS 170 prerequistie:\n Computer Science 61B and 70.");
+$("#CS-170").attr("title", "CS 170 prerequisite:\n Computer Science 61B and 70.");
 
-$("#CS-188").attr("title", "CS188 prerequisite:\n Computer Science 61A or 61B and consent of instructor; Computer Science 70.");
+$("#CS-188").attr("title", "CS 188 prerequisite:\n Computer Science 61A or 61B and consent of instructor; Computer Science 70.");
