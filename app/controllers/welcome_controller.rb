686cae80acee4c0619dca3214baa0206f7277888
Alex Chen <chen.alex@berkley.edu>
2013-12-06 17:18:36 -0800

    added some jobs to outlook graph

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,16 +9,13 @@ class WelcomeController < ApplicationController
 	end 
 	
 	def outlook
-    @graph = LazyHighCharts::HighChart.new('bar') do |f|
+      @graph = LazyHighCharts::HighChart.new('bar') do |f|
       f.options[:chart][:defaultSeriesType] = "bar"
-      f.series(:name=>"Salary", :data=> [100,12,30])
-      f.series(:name=>"Openings", :data => [10,1,3])
-      f.title({:text=>"Job Outlook"})
-      f.subtitle(:text => 'Source: Wikipedia.org')
-       
-
-      f.options[:xAxis][:categories] = ['Software Engineer','Teacher','Laywer']
-      f.options[:yAxis][:title][:text] = 'Salary ($)' 
+      f.series(:name=>"Salary in $1000", :data=> [65,53,67,65,88,82,91,67])
+      f.series(:name=>"Projected Growth (%)", :data => [26,17,32,17,27,62,30,37])
+      f.title({:text=>"Career Outlook"})
+      f.subtitle(:text => 'Source: Bureau of Labor Statistics')
+      f.options[:xAxis][:categories] = ['Nurse','Elementary School Teacher','Financial Planner', 'Technicial Writer','Actuary','Biomedical Engineer','Software Engineer','Audiologist']
      end
 	end 
 end
