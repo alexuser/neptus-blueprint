4b1a00d383baf31c12bab36923d6d9c377535c77
Kaiqi <cenkaiqi@gmail.com>
2013-12-07 15:23:04 -0800

    update popover

--- app/assets/javascripts/popover.js
+++ app/assets/javascripts/popover.js
@@ -1,46 +1,132 @@
-$("#CS-61A").attr("title", "CS 61A prerequisite:\n Mathematics 1A (may be taken concurrently); programming experience equivalent to that gained in 3 or the Advanced Placement Computer Science A course.");
-
-$("#CS-61B").attr("title", "CS 61B prerequisite:\n 61A or Engineering 7.");
-
-$("#CS-61C").attr("title", "CS 61C prerequisite:\n 61A, along with either 61B or 61BL, or programming experience equivalent to that gained in 9C, 9F, or 9G.");
-
-$("#Math-1A").attr("title", "Math 1A prerequisite:\n Three and one-half years of high school math, including trigonometry and analytic geometry, plus a satisfactory grade in one of the following: CEEB MAT test, an AP test, the UC/CSU math diagnostic test, or 32. Consult the mathematics department for details. Students with AP credit should consider choosing a course more advanced than 1A.");
-
-$("#Math-1B").attr("title", "Math 1B prerequisite:\n Math 1A");
-
-$("#Math-54").attr("title", "Math 54 prerequisite:\n Math 1B");
-
-$("#CS-70").attr("title", "CS 70 prerequisite:\n Sophomore mathematical maturity, and programming experience equivalent to that gained in 3 or the Advanced Placement Computer Science A course.");
-
-$("#EE-20").attr("title", "EE 20 prerequisite:\n Mathematics 1B.");
-
-$("#EE-40").attr("title", "EE 40 prerequisite:\n Mathematics 1B.");
-
-$("#EE-42").attr("title", "EE 42 prerequisite:\n Mathematics 1B.");
-
-$("#CS-C149").attr("title", "CS C149 prerequisite:\n 20N; Computer Science 61C; Computer Science 70 or Math 55.");
-
-$("#CS-150").attr("title", "CS 150 prerequisite:\n CS 61C, Electrical Engineering 40 or 42.");
-
-$("#CS-152").attr("title", "CS 152 prerequisite:\n CS 61C.");
-
-$("#CS-160").attr("title", "CS 160 prerequisite:\n Computer Science 61B or 61BL.");
-
-$("#CS-162").attr("title", "CS 162 prerequisite:\n Computer Science 61B, 61C, and 70.");
-
-$("#CS-164").attr("title", "CS 164 prerequisite:\n 61B and 61C.");
-
-$("#CS-169").attr("title", "CS 169 prerequisite:\n Computer Science 61B and 61C, and either Computer Science 70 or Mathematics 113.");
-
-$("#CS-184").attr("title", "CS 184 prerequisite:\n Computer Science 61B or 61BL; programming skills in C, C++, or Java; linear algebra and calculus.");
-
-$("#CS-186").attr("title", "CS 186 prerequisite:\n 61B and 61C.");
-
-$("#CS-161").attr("title", "CS 161 prerequisite:\n 61C (Machine Structures), plus either 70 (Discrete Mathematics) or Mathematics 55.");
-
-$("#CS-170").attr("title", "CS 170 prerequisite:\n Computer Science 61B and 70.");
-
-$("#CS-188").attr("title", "CS 188 prerequisite:\n Computer Science 61A or 61B and consent of instructor; Computer Science 70.");
+$("#CS-61A").attr("title", "CS 61A prerequisite");
+$("#CS-61A").popover({
+	trigger: 'hover',
+	content: 'Mathematics 1A (may be taken concurrently); programming experience equivalent to that gained in 3 or the Advanced Placement Computer Science A course'
+});
 
 
-$("#foobar").popover();
+$("#CS-61B").attr("title", "CS 61B prerequisite");
+$("#CS-61B").popover({
+	trigger: 'hover',
+	content: 'CS61A or Engineering 7'
+});
+
+$("#CS-61C").attr("title", "CS 61C prerequisite");
+$("#CS-61C").popover({
+	trigger: 'hover',
+	content: '61A, along with either 61B or 61BL, or programming experience equivalent to that gained in 9C, 9F, or 9G'
+});
+
+$("#Math-1A").attr("title", "Math 1A prerequisite");
+$("#Math-1A").popover({
+	trigger: 'hover',
+	content: 'Three and one-half years of high school math, including trigonometry and analytic geometry, plus a satisfactory grade in one of the following: CEEB MAT test, an AP test, the UC/CSU math diagnostic test, or 32. Consult the mathematics department for details. Students with AP credit should consider choosing a course more advanced than 1A'
+});
+
+$("#Math-1B").attr("title", "Math 1B prerequisite");
+$("#Math-1B").popover({
+	trigger: 'hover',
+	content: 'Math 1A'
+});
+
+$("#Math-54").attr("title", "Math 54 prerequisite");
+$("#Math-54").popover({
+	trigger: 'hover',
+	content: 'Math 1B'
+});
+
+$("#CS-70").attr("title", "CS 70 prerequisite");
+$("#CS-70").popover({
+	trigger: 'hover',
+	content: 'Sophomore mathematical maturity, and programming experience equivalent to that gained in 3 or the Advanced Placement Computer Science A course'
+});
+
+$("#EE-20").attr("title", "EE 20 prerequisite");
+$("#EE-20").popover({
+	trigger: 'hover',
+	content: 'Mathematics 1B'
+}) ;
+
+$("#EE-40").attr("title", "EE 40 prerequisite");
+$("#EE-40").popover({
+	trigger: 'hover',
+	content: 'Mathematics 1B'
+}); 
+
+$("#EE-42").attr("title", "EE 42 prerequisite");
+$("#EE-42").popover({
+	trigger: 'hover',
+	content: 'Mathematics 1B'
+});
+
+$("#CS-C149").attr("title", "CS C149 prerequisite");
+$("#CS-C149").popover({
+	trigger: 'hover',
+	content: 'EE20N; Computer Science 61C; Computer Science 70 or Math 55'
+}); 
+
+$("#CS-150").attr("title", "CS 150 prerequisite");
+$("#CS-150").popover({
+	trigger: 'hover',
+	content: 'CS 61C, Electrical Engineering 40 or 42'
+});
+
+$("#CS-152").attr("title", "CS 152 prerequisite");
+$("#CS-152").popover({
+	trigger: 'hover',
+	content: 'CS 61C'
+});
+
+$("#CS-160").attr("title", "CS 160 prerequisite");
+$("#CS-160").popover({
+	trigger: 'hover',
+	content: 'Computer Science 61B or 61BL'
+}); 
+
+$("#CS-162").attr("title", "CS 162 prerequisite");
+$("#CS-162").popover({
+	trigger: 'hover',
+	content: 'Computer Science 61B, 61C, and 70'
+});
+
+$("#CS-164").attr("title", "CS 164 prerequisite");
+$("#CS-164").popover({
+	trigger: 'hover',
+	content: 'CS61B and CS61C'
+});
+
+$("#CS-169").attr("title", "CS 169 prerequisite");
+$("#CS-169").popover({
+	trigger: 'hover',
+	content: 'Computer Science 61B and 61C, and either Computer Science 70 or Mathematics 113'
+});
+
+$("#CS-184").attr("title", "CS 184 prerequisite");
+$("#CS-184").popover({
+	trigger: 'hover',
+	content: 'Computer Science 61B or 61BL; programming skills in C, C++, or Java; linear algebra and calculus'
+});
+
+$("#CS-186").attr("title", "CS 186 prerequisite");
+$("#CS-186").popover({
+	trigger: 'hover',
+	content: 'CS61B and CS61C'
+});
+
+$("#CS-161").attr("title", "CS 161 prerequisite");
+$("#CS-161").popover({
+	trigger: 'hover',
+	content: 'CS61C (Machine Structures), plus either CS70 (Discrete Mathematics) or Mathematics 55'
+});
+
+$("#CS-170").attr('title', 'CS 170 prerequisites');
+$("#CS-170").popover({
+	trigger: 'hover',
+	content: 'Computer Science 61B and 70'
+});
+$("#CS-188").attr('title', 'CS 188 prerequisites');
+$("#CS-188").popover({
+	trigger: 'hover',
+	content: 'Computer Science 61A or 61B and consent of instructor; Computer Science 70.'
+});
+
