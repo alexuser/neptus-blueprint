e76806661c20471387f354304f2e306507200619
Alex Chen <chen.alex@berkley.edu>
2013-12-06 18:59:19 -0800

    updated forum jpeg, removed unused methods in mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,9 @@ class UserMailer < ActionMailer::Base
   default :from => "no-reply@example.com"
 
   def password_reset(user, password)
-    @user = user
-    @password = password
-    mail(:to => user.email,
-         :subject => 'Password Reset Notification')
+    # @user = user
+    # @password = password
+    # mail(:to => user.email,
+    #      :subject => 'Password Reset Notification')
   end
 end
